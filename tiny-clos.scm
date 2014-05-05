b84b552c57ec2b641ee94e08ecff7bdb352a1605
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-05-05 19:05:38 +0900

    fix load path

--- tiny-clos.scm
+++ tiny-clos.scm
@@ -187,7 +187,7 @@
 ; my understanding of the CL world...  I hope it is right.
 ;
 ;
-(load "support")
+(load "support.scm")
 
 ;
 ; Then, we need to build what, in a more real implementation, would be
